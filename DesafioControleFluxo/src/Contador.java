8067db3652191f8f57280af852fceb250053a480
Melissa <santossmelissa1@gmail.com>
2024-06-10 14:31:19 -0300

    Desafio: Controle de Fluxo

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args) {
-        Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.nextInt();
+            Scanner terminal = new Scanner(System.in);
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
 		
 		try {
 			//chamando o método contendo a lógica de contagem
