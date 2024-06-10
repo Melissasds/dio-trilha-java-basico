4c4380eb9942278d7f1dd65d436243b3d4d9834a
Melissa <santossmelissa1@gmail.com>
2024-06-10 14:37:29 -0300

    Desafio: Controle de Fluxo

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -19,13 +19,13 @@ public class Contador {
 		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+            //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
             if (parametroUm >= parametroDois){
                 throw new ParametrosInvalidosException();
         }
 		
 		int contagem = parametroDois - parametroUm;
-		//imprime os números com base na variável contagem
+		    //imprime os números com base na variável contagem
             for (int i =  1; i <= contagem; i++){
                 System.out.println("Imprimindo o número " + i);
             }
